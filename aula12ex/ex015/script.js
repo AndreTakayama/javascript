c2ba24b9d985da5722d0822c9e33d4e444ba0501
André Takayama <105654259+AndreTakayama@users.noreply.github.com>
2022-07-29 11:51:00 -0300

    aula12full

--- aula12ex/ex015/script.js
+++ aula12ex/ex015/script.js
@@ -10,6 +10,41 @@ function verificar () {
         var idade = ano - Number(fano.value)
         res.innerHTML = `Idade calculada: ${idade}`
         var genero = ''
-        
+        var img = document.createElement('img')
+        img.setAttribute('id', 'foto')
+        if (fsex[0].checked) {
+            gênero = 'Homem'
+            if (idade >=0 && idade < 12){
+                'Criança'
+                    img.setAttribute('src', 'foto-bebe-m.png')
+                } else if (idade < 18) {
+                    'Adolescente'
+                    img.setAttribute('src', 'foto-jovem-m.png')
+                } else if (idade < 60) {
+                    'Adulto'
+                    img.setAttribute('src', 'foto-adulto-m.png')
+                } else {
+                    'Idoso'
+                    img.setAttribute('src', 'foto-idoso-m.png')
+                }
+
+            } else if (fsex[1].checked) {
+                gênero = 'mulher' 
+                if (idade >=0 && idade < 12){
+                    'Criança'
+                    img.setAttribute('src', 'foto-bebe-f.png')
+                    } else if (idade < 22) {
+                        'Adolescente'
+                        img.setAttribute('src', 'foto-jovem-f.png')
+                    } else if (idade < 60) {
+                        'Adulta'
+                        img.setAttribute('src', 'foto-adulto-f.png')
+                    } else {
+                        'Idosa'
+                        img.setAttribute('src','foto-idoso-f.png')
+                    }
+        }
+        res.innerHTML = `Detectamos ${gênero} com ${idade} anos.`
+        res.appendChild(img)
     }
 }
---
+++ aula13/ambiente.js
@@ -0,0 +1,29 @@
+var c = 1
+do {
+    console.log(`passo ${c}`)
+    c++
+} while (c <=7)
+
+
+
+
+
+
+/*var c = 1
+while (c <= 6) {
+    console.log(`passo ${c}`)
+    c++
+}*/
+
+
+
+
+
+/*
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+*/
